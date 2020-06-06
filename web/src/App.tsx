ba9dc599f7bcb0f8754c9130139e917cb625793d
Felipe Silva <felipe.salaberga@gmail.com>
2020-06-05 23:23:43 -0300

    Finished app Ecoleta

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <>
+      <ToastContainer autoClose={3000} />
+      <Routes />
+    </>
+  );
+}
+
+export default App;
